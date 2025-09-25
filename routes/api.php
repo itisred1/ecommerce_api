67029dc8b20302881df867d8cbcf28e4c337396d
rdmute <rdmute8@gmail.com>
2025-09-25 15:45:36 +0100

    Added user auth with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use GuzzleHttp\Middleware;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,11 +16,19 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
 // Categories
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/products', [CategoryController::class, 'groupByCategory']);
+
+// Users
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
